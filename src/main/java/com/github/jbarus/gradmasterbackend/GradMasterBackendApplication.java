fa3c40f011ce0a9d8ac3b86c5ce1c93e7d4341a9
jbarus <57799873+jbarus@users.noreply.github.com>
2024-09-23 19:11:03 +0200

    Batman

---
+++ src/main/java/com/github/jbarus/gradmasterbackend/GradMasterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.jbarus.gradmasterbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradMasterBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradMasterBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/jbarus/gradmasterbackend/GradMasterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jbarus.gradmasterbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradMasterBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
