0fe09f09d42b21d29d0aa8cad8f191875488da6f
jbarus <57799873+jbarus@users.noreply.github.com>
2024-10-09 18:41:34 +0200

    Add dependencies and test implementation for gRPC and Protobuf

--- src/main/java/com/github/jbarus/gradmasterbackend/controllers/ContextController.java
+++ src/main/java/com/github/jbarus/gradmasterbackend/controllers/ContextController.java
@@ -1,8 +1,13 @@
 package com.github.jbarus.gradmasterbackend.controllers;
 
+import com.github.jbarus.HelloRequest;
+import com.github.jbarus.HelloResponse;
+import com.github.jbarus.HelloServiceGrpc;
 import com.github.jbarus.gradmasterbackend.models.dto.ContextDTO;
 
 import com.github.jbarus.gradmasterbackend.services.ContextService;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,5 +35,23 @@ public class ContextController {
     @GetMapping("/{id}")
     public ResponseEntity<ContextDTO> getContextById(@PathVariable UUID id) {
         return ResponseEntity.ok().body(contextService.getContextById(id));
+    }
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello() {
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 9090)
+                .usePlaintext()
+                .build();
+
+        HelloServiceGrpc.HelloServiceBlockingStub stub
+                = HelloServiceGrpc.newBlockingStub(channel);
+
+        HelloResponse helloResponse = stub.hello(HelloRequest.newBuilder()
+                .setFirstName("Baeldung")
+                .setLastName("gRPC")
+                .build());
+
+        channel.shutdown();
+        return ResponseEntity.ok().body(helloResponse.getGreeting());
     }
 }
